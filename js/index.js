e6b2913edc1a60b25f18bac39d5ed34ab6de63ce
Shengbin <shengbin.cui.1230@gmail.com>
2023-07-07 23:37:27 +0900

    added two lines of js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,4 @@
 
-alert("Hello, there. ")
+document.getElementsByClassName("cover-date")[0].textContent = "July 7th, 2023";
+
+document.querySelector(".latest-description").style.color = "rgb(229,229,203)";
